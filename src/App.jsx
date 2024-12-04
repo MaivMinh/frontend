50b7bc6c402cab72f906f7a038aa7e37c2fd4a75
MaivMinh <maivanminh.se@gmail.com>
2024-12-05 00:28:00 +0700

    Add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css';
-import React from 'react';
-import { createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import { createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import { AppContextProvider } from './AppContext';
-import PrivateRoute from './components/PrivateRoute';
-import GoogleRedirected from './components/GoogleRedirected';
+import "./App.css";
+import React from "react";
+import {
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import { AppContextProvider } from "./AppContext";
+import PrivateRoute from "./components/PrivateRoute";
+import GoogleRedirected from "./components/GoogleRedirected";
 import FacebookOauth2RedirectHandler from "./components/FacebookOauth2RedirectHandler";
-import UnauthorizedPage from './pages/Unauthorize';
+import UnauthorizedPage from "./pages/Unauthorize";
 
 const routes = createRoutesFromElements(
   <>
     <Route path="/" element={<Home />} />,
     <Route path="/home" element={<Home />} />
-      <Route path="/oauth2/google/redirected" element={<GoogleRedirected />}  />
-
+    <Route path="/oauth2/google/redirected" element={<GoogleRedirected />} />
     <Route path="/login" element={<Login />} />,
     <Route path="/register" element={<Register />} />
     <Route element={<PrivateRoute />}>
@@ -27,7 +30,7 @@ const routes = createRoutesFromElements(
       path="/oauth2/facebook/redirect"
       element={<FacebookOauth2RedirectHandler />}
     />
-    <Route path='/unauthorize' element={<UnauthorizedPage />} />
+    <Route path="/unauthorize" element={<UnauthorizedPage />} />
   </>
 );
 
